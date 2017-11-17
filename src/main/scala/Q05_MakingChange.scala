f502240812ce7244fa224c73a27959a3fa403588
Angel Ortega <cs.angelortega@gmail.com>
2017-11-16 17:47:46 -0800

    Write the initial problem statement and objective criteria for the coding question I am solving, Question 5

--- src/main/scala/Q05_MakingChange.scala
+++ src/main/scala/Q05_MakingChange.scala
@@ -1,11 +1,35 @@
+/**
+  * # __Your quirky boss collects rare, old coins...__
+  *
+  * They found out you're a programmer and asked you to
+  * solve something they've been wondering for a long time.
+  *
+  * Write a function that, given:
+  *
+  *   1. an `amount` of money
+  *   2. a list of coin `denominations`
+  *
+  * computes the number of ways to make the amount of money
+  * with coins of the available `denominations`.
+  *
+  * __Example__: for `amount` = 4 (4 cents) and
+  * denominations = [1, 2, 3] (1 cent, 2 cent, 3 cent),
+  * your program would output 4 -- the number of ways to make
+  * 4 cents with those `denominations`:
+  *
+  *   1. 1 cent, 1 cent, 1 cent, 1 cent
+  *   2. 1 cent, 1 cent, 2 cent
+  *   3. 1 cent, 3 cent
+  *   4. 2 cent, 2 cent
+  *
+  */
 object Q05_MakingChange extends App {
 
-  val denominations = List(1, 2, 3, 4)
-  val coin = 4
+  val denominations = Array(1, 2, 3, 4)
+  val amount = 4
 
-  def makeChange(denominations: List[Int]) = {
+  val makeChange = () => (denominations: Array[Int]) = {
 
   }
 
-  // Memoize
 }
